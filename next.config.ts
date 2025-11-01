74e034fbee6bfcc5058895d5723ec1a6e8ce8abf
Elena Polyakova <epolyk1970@gmail.com>
2025-11-01 19:54:13 +0200

    .env

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,20 @@
 import type { NextConfig } from 'next';
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: 'ac.goit.global' }],
   },
   reactStrictMode: true,
   async rewrites() {
+    const apiUrl =
+      process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+        destination: apiUrl + '/:path*',
       },
     ];
   },
 };
+
 export default nextConfig;
