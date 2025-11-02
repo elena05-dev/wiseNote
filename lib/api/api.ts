b5c5d60f9706a038264c2564cc15282424ba05a7
Elena Polyakova <epolyk1970@gmail.com>
2025-11-02 19:54:13 +0200

    api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const nextServer = axios.create({
-  baseURL: 'https://wise-note-backend.onrender.com',
+  baseURL: 'https://notehub-backend-rj4e.onrender.com/',
   withCredentials: true,
 });
