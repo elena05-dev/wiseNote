6b4b0893211639a0e9fe1b04dc84fd66771b5cb5
Elena Polyakova <epolyk1970@gmail.com>
2025-11-03 20:33:43 +0200

    port render

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const nextServer = axios.create({
-  baseURL: 'https://notehub-backend-rj4e.onrender.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
