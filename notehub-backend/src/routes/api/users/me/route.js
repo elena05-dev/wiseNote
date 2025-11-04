7acf856d664d3e6c23c384a83549ed4bfa0f341b
Elena Polyakova <epolyk1970@gmail.com>
2025-11-04 22:40:28 +0200

    m

--- notehub-backend/src/routes/api/users/me/route.js
+++ notehub-backend/src/routes/api/users/me/route.js
@@ -4,9 +4,6 @@ import { updateUserController } from '../../../../controllers/auth.js';
 const router = Router();
 
 router.get('/me', authenticate, async (req, res) => {
-  console.log('=== /users/me called ===');
-  console.log('Cookies:', req.cookies);
-  console.log('Headers:', req.headers.authorization);
   const user = req.user;
   res.json({ _id: user._id, name: user.name, email: user.email });
 });
