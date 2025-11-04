3491b51ad7684ea48b580f2eeb3ec8f18a8c0252
Elena Polyakova <epolyk1970@gmail.com>
2025-11-04 16:14:01 +0200

    error 500

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ hostname: 'ac.goit.global' }],
+    remotePatterns: [
+      {
+        hostname: 'ac.goit.global',
+      },
+    ],
   },
-  reactStrictMode: true,
 };
 
 export default nextConfig;
