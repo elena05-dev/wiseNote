6b4380e29255a16773b0f795d1978d952c4f950a
Elena Polyakova <epolyk1970@gmail.com>
2026-08-07 17:51:56 +0300

    debug middleware redirect

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,21 +1,5 @@
-//import { cookies } from 'next/headers';
-//import { redirect } from 'next/navigation';
-//import PrivateLayoutClient from './PrivateLayoutClient';
-
-//export default async function Layout({
-// children,
-//}: {
-//  children: React.ReactNode;
-//}) {
-//const cookieStore = await cookies();
-//const sessionId = cookieStore.get('sessionId')?.value;
-
-//if (!sessionId) {
-//   redirect('/sign-in');
-// }
-
-// return <PrivateLayoutClient>{children}</PrivateLayoutClient>;
-//}
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import PrivateLayoutClient from './PrivateLayoutClient';
 
 export default async function Layout({
@@ -23,5 +7,12 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get('sessionId')?.value;
+
+  if (!sessionId) {
+    redirect('/sign-in');
+  }
+
   return <PrivateLayoutClient>{children}</PrivateLayoutClient>;
 }
